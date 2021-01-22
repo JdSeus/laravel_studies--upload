51551724b052f1393ab0196f6dc03129e468fe31
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-22 11:22:56 -0300

    preparacao do bando de dados

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+use App\Http\Controllers\PostControlador;
+
+Route::get('/', [PostControlador::class, 'index']);
